c2259e95fad7b4cbfe2be1d031729f725ec15ab9
Shubham Singh <shubhamsinghia160@gmail.com>
2025-03-30 00:27:27 +0530

    Update cloudinary.js

--- src/utils.js/cloudinary.js
+++ src/utils.js/cloudinary.js
@@ -2,9 +2,9 @@ import { v2 as cloudinary } from 'cloudinary'
 import fs from 'fs'
 
 cloudinary.config({
-    cloud_name: 'dycmay6eq',
-    api_key: '266674131275262',
-    api_secret: 'Gc5t9Dq-e-_9YHYQylfK7GeaNsI'
+    cloud_name: '',
+    api_key: '',
+    api_secret: ''
     // cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
     // api_key:process.env.CLOUDINARY_API_KEY,
     // api_secret:process.env.CLOUDINARY_API_SECRET
