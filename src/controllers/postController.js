2ef03e495f519e0c9b56e7202abf11a1274cf1d8
shubham singhk <shubhamsinghia160@gmail.com>
2025-04-04 16:02:53 +0530

    feat: add functions to retrieve single post and user posts, and update post route

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -1,5 +1,5 @@
 import { Post } from "../models/postModel.js";
-// import { ApiError } from "../utils.js/ApiError.js";
+import { ApiError } from "../utils.js/ApiError.js";
 import { ApiResponse } from "../utils.js/apiResponse.js";
 
 const createPost = async (req, res) => {
@@ -47,13 +47,85 @@ const createPost = async (req, res) => {
     }
 }
 
-// Get all posts
 const getAllPosts = async (req, res) => {
 
-    
+    try {
+        const posts = await Post.find({}).populate("author", "name email profilePicture").sort({ createdAt: -1 })
+
+        if (!posts) {
+            return res.status(404).json({ message: "No posts found" });
+        }
+        return res.status(200).json(
+            new ApiResponse(200, posts, "Posts fetched successfully")
+        )
+    } catch (error) {
+        return res.status(500).json(
+            new ApiError(
+                500,
+                "Internal server error",
+                error.message || "Something went wrong"
+            )
+        )
+
+    }
+
+
+
+
 }
 
+const getSinglePost = async (req, res) => {
+    try {
+        const { id } = req.params
+        const post = await Post.findById(id).populate("author", "name email profilePicture")
+        if (!post) {
+            return res.status(404).json({ message: "Post not found" });
+        }
+        return res.status(200).json(
+            new ApiResponse(200, post, "Post fetched successfully")
+        )
+    } catch (error) {
+        return res.status(500).json(
+            new ApiError(
+                500,
+                "Internal server error",
+                error.message || "Something went wrong"
+            )
+        )
+
+    }
+}
+
+const getUserPosts = async (req, res) => {
+    try {
+        const userId = req.user._id
+        const posts = await Post.find({ author: userId }).populate("author", "name email profilePicture").sort({ createdAt: -1 })
+        if (!posts) {
+            return res.status(404).json({ message: "No posts found" });
+        }
+        return res.status(200).json(
+            new ApiResponse(200, posts, "Posts fetched successfully")
+        )
+    } catch (error) {
+        return res.status(500).json(
+            new ApiError(
+                500,
+                "Internal server error",
+                error.message || "Something went wrong"
+            )
+        )
+
+    }
+
+}
+
+const updatePost = async(req, res) => {
+   
+}
 export {
     createPost,
     getAllPosts,
+    getSinglePost,
+    getUserPosts,
+    updatePost,
 }
--- src/routes/postRoute.js
+++ src/routes/postRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createPost } from "../controllers/postController.js";
+import { createPost, getAllPosts, getSinglePost, getUserPosts, updatePost } from "../controllers/postController.js";
 import { authenticateUser } from "../middlewares/authMiddleware.js";
 import { upload } from "../middlewares/multerMiddleware.js";
 
@@ -7,6 +7,10 @@ import { upload } from "../middlewares/multerMiddleware.js";
 const postRouter = express.Router();
 
 postRouter.route("/create").post(authenticateUser, upload.single("postPicture"), createPost);
+postRouter.route("/get-all-posts").get(authenticateUser, getAllPosts);
+postRouter.route("/getSinglePost/:id").get(authenticateUser, getSinglePost);
+postRouter.route("/getUserPosts").get(authenticateUser, getUserPosts);
+postRouter.route("/updatePost/:id").get(authenticateUser, updatePost);
 
 
 export default postRouter;
